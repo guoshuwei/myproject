a1599d480420f244a2ee4b21bb6b60f70d640b97
root <root@debian.debian>
2018-11-20 00:43:57 -0500

    add not .git smarty

---
+++ user/smarty/templates_c/bedd8cf67f1b4dfc049226d4145306d73b91a360_0.file.footer.tpl.cache.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.34-dev-5, created on 2018-10-24 11:44:03
+  from '/var/www/otc_option/smarty/templates/footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-5',
+  'unifunc' => 'content_5bd05b036ffe80_37186794',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'bedd8cf67f1b4dfc049226d4145306d73b91a360' => 
+    array (
+      0 => '/var/www/otc_option/smarty/templates/footer.tpl',
+      1 => 1540380356,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bd05b036ffe80_37186794 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '4381388275bd05b036feca2_70928892';
+?>
+</BODY>
+</HTML>
+<?php }
+}
