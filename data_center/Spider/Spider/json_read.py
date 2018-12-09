8ec093ac29670ad062978ffbb965c0e6f2c11cea
root <root@debian.debian>
2018-12-09 13:04:28 -0500

    delete

--- data_center/Spider/Spider/json_read.py
+++
@@ -1,4 +0,0 @@
-import json
-with open('input.json') as json_data
-	data = json.load(json_data)
-	print(data)
--- user/static/scripts/build.bat
+++
@@ -1,7 +0,0 @@
-node deploy.js -m page/member_invests
-node deploy.js -m page/apanel
-node deploy.js -m page/apanel_add
-node deploy.js -m page/reg
-node deploy.js -m page/index
-
-PAUSE
